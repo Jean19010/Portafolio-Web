6765274a4b8bd48433de546bc5e6a06738c21048
Jean Alvarez <107156851+Jean19010@users.noreply.github.com>
2022-09-02 23:02:45 -0500

    Portafolio web- beta
    Version 0.1

---
+++ portafolio_web/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portafolio web</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <script src="https://kit.fontawesome.com/9c5f1d9041.js" crossorigin="anonymous"></script>
+</head>
+<body id="body">
+    
+    <header>
+        <div class="icon_menu">
+            <i class="fa-solid fa-bars" id="btn_open"></i>
+        </div>
+    </header>
+
+    <div class="menu__side" id="menu_side">
+        <div class="name_page">
+            <i class="fa-solid fa-code"></i>
+            <h4>Paolo_Alvarez</h4>
+        </div>
+
+        <div class="options_menu">
+
+            <a href="#" class="selected">
+                <div class="option">
+                    <i class="fas fa-home" title="Inicio"></i>
+                    <h4>Inicio</h4>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="option">
+                    <i class="fas fa-file" title="Portafolio"></i>
+                    <h4>Portafolio</h4>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="option">
+                    <i class="fas fa-video" title="Cursos"></i>
+                    <h4>Cursos</h4>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="option">
+                    <i class="fas fa-sticky-note" title="blog"></i>
+                    <h4>Blog</h4>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="option">
+                    <i class="fa-regular fa-address-book" title="Contacto"></i>
+                    <h4>Contacto</h4>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="option">
+                    <i class="fa-regular fa-address-card" title="Nosotros"></i>
+                    <h4>Nosotros</h4>
+                </div>
+            </a>
+
+         </div>
+
+    </div>
+
+    <main>
+        <div class="inicio_page" id="div_inicio" style="display:none">
+            <h1>Soy Desarrollador Web</h1>
+            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae temporibus hic necessitatibus, 
+                eveniet beatae nemo amet. Eius provident expedita eum perspiciatis quisquam adipisci modi quo alias 
+                nobis molestias dignissimos velit blanditiis ipsa repellendus architecto ea impedit, aut placeat 
+                exercitationem qui pariatur doloremque dolore! Similique iure dolor vero impedit, neque temporibus 
+                animi reprehenderit consectetur voluptas mollitia fugiat, exercitationem non pariatur numquam.
+            </p> <br>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur cum deleniti voluptatum voluptatem veniam. Eligendi reiciendis enim consequuntur fugit aut quae, iure quidem sint quibusdam culpa esse nihil ab amet!</p>
+        </div>
+
+        <div class="portafolio_page" >
+            <h2>Este es mi portafolio</h2>
+            <p>Mis proyectos se basan en distintas tecnologias, etc.</p>
+        </div>
+    </main>
+
+
+    <script src="js/script.js"></script>
+
+</body>
+</html>
